322599aa810dc398dad8cc5641c9703ca557507a
Devesh Mishra <124690118+devmishra921@users.noreply.github.com>
2025-07-30 11:41:37 +0530

    db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // db_connect.php
-$host = 'localhost';
-$user = 'root';
-$pass = '';          // ← your MySQL password
-$db   = 'rr_business';
+$host = 'sql110.infinityfree.com';
+$user = 'if0_39236898';
+$pass = 'Rrbusiness2025';
+$db   = 'if0_39236898_rrbusiness'; 
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
