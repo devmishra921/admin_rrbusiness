34354f563c02b75a92b0fdab3facdb5e619e157e
Devesh Mishra <124690118+devmishra921@users.noreply.github.com>
2025-07-30 12:40:32 +0530

    db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,10 +3,10 @@
 $host = 'sql110.infinityfree.com';
 $user = 'if0_39236898';
 $pass = 'Rrbusiness2025';
-$db   = 'if0_39236898_rrbusiness'; 
+$db   = 'if0_39236898_rrbusiness';  // जो तूने बनाया है
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
-    die('Database connection failed: ' . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
